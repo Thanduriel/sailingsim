3ff5390adebf8211d991cea958d0c82f464bd443
Robert Jendersie <rojendersie@alice.de>
2025-04-12 16:26:25 +0200

    added bevy_water demo

---
+++ src/app.rs
@@ -0,0 +1,429 @@
+// basically just the bevy_water demo (https://github.com/Neopallium/bevy_water)
+//! Showcases dynamic ocean material + dynamic Sun/Atmosphere.
+//! Most of the daylight cycle code taken from the examples of `bevy_atmosphere`.
+
+#[cfg(feature = "depth_prepass")]
+use bevy::core_pipeline::prepass::DepthPrepass;
+
+use bevy::core_pipeline::Skybox;
+
+use bevy::pbr::wireframe::{Wireframe, WireframePlugin};
+use bevy::render::mesh::*;
+use bevy::{
+    app::AppExit,
+    prelude::*,
+    render::{mesh::VertexAttributeValues, render_resource::TextureFormat},
+};
+
+#[cfg(feature = "panorbit")]
+use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin};
+
+use bevy_spectator::*;
+
+#[cfg(feature = "debug")]
+use bevy_prototype_debug_lines::{DebugLines, DebugLinesPlugin};
+
+use bevy_water::*;
+
+pub const WATER_HEIGHT: f32 = 1.0;
+
+pub fn pirates_app(title: &str) -> App {
+    let mut app = App::new();
+    app
+        // Tell the asset server to watch for asset changes on disk:
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: title.to_string(),
+                        resolution: (1200., 600.).into(),
+                        ..Default::default()
+                    }),
+                    ..default()
+                })
+                .set(AssetPlugin::default()),
+        );
+
+    #[cfg(feature = "debug")]
+    app.add_plugins(DebugLinesPlugin::with_depth_test(true));
+
+    //   #[cfg(feature = "inspector")]
+    //   app.add_plugins(bevy_inspector_egui::quick::WorldInspectorPlugin::new());
+
+    app.add_plugins(SpectatorPlugin);
+
+    // Simple pan/orbit camera.
+    #[cfg(feature = "panorbit")]
+    app.add_plugins(PanOrbitCameraPlugin);
+
+    // Improve shadows.
+    app.insert_resource(bevy::pbr::DirectionalLightShadowMap { size: 4 * 1024 })
+        // Water
+        .insert_resource(WaterSettings {
+            height: WATER_HEIGHT,
+            ..default()
+        })
+        .add_plugins((WaterPlugin, ImageUtilsPlugin))
+        // Ship Physics.
+        .add_systems(Update, update_ships)
+        // Quit on `Q` press.
+        .add_systems(Update, handle_quit)
+        // Wireframe
+        .add_plugins(WireframePlugin)
+        .init_resource::<UiState>()
+        .add_systems(Update, toggle_wireframe);
+
+    app
+}
+
+#[allow(dead_code)]
+pub fn main() {
+    let mut app = pirates_app("Pirates");
+
+    // Setup
+    app.add_systems(Startup, (setup_ocean, setup_orb, setup_camera, setup_ships));
+
+    app.run();
+}
+
+pub fn handle_quit(input: Res<ButtonInput<KeyCode>>, mut exit: EventWriter<AppExit>) {
+    if input.pressed(KeyCode::KeyQ) {
+        exit.send(AppExit::Success);
+    }
+}
+
+#[derive(Resource, Clone, Debug, Default)]
+pub struct UiState {
+    show_wireframe: bool,
+}
+
+pub fn toggle_wireframe(
+    input: Res<ButtonInput<KeyCode>>,
+    query: Query<Entity, With<Mesh3d>>,
+    mut commands: Commands,
+    mut state: ResMut<UiState>,
+) {
+    if input.just_pressed(KeyCode::KeyR) {
+        // Update flag.
+        let show_wireframe = !state.show_wireframe;
+        state.show_wireframe = show_wireframe;
+
+        for entity in query.iter() {
+            let mut entity = commands.entity(entity);
+            if show_wireframe {
+                entity.insert(Wireframe);
+            } else {
+                entity.remove::<Wireframe>();
+            }
+        }
+    }
+}
+
+// Marker for updating the position of the light, not needed unless we have multiple lights
+#[derive(Component)]
+pub struct Sun;
+
+#[derive(Component, Default, Clone)]
+#[require(Transform, Visibility)]
+pub struct Ship {
+    water_line: f32,
+    front: Vec3,
+    back_left: Vec3,
+    back_right: Vec3,
+}
+
+impl Ship {
+    pub fn new(water_line: f32, front: f32, back: f32, left: f32, right: f32) -> Self {
+        Self {
+            water_line,
+            front: Vec3::new(0.0, 0.0, front),
+            back_left: Vec3::new(left, 0.0, back),
+            back_right: Vec3::new(right, 0.0, back),
+        }
+    }
+
+    fn update(
+        &self,
+        water: &WaterParam,
+        pos: Vec3,
+        transform: &mut Transform,
+        #[cfg(feature = "debug")] lines: &mut DebugLines,
+    ) {
+        let (yaw, _pitch, _roll) = transform.rotation.to_euler(EulerRot::YXZ);
+        let global = Transform::from_translation(pos).with_rotation(Quat::from_rotation_y(yaw));
+
+        // Get the wave position at the front, back_left and back_right.
+        let mut front = water.wave_point(global.transform_point(self.front));
+        let left = water.wave_point(global.transform_point(self.back_left));
+        let right = water.wave_point(global.transform_point(self.back_right));
+        let normal = (left - front).cross(right - front).normalize();
+
+        // Debug lines.
+        #[cfg(feature = "debug")]
+        {
+            lines.line(front, front + normal, 0.0);
+            lines.line_colored(front, right, 0.0, Color::RED);
+            lines.line(right, left, 0.0);
+            lines.line_colored(left, front, 0.0, Color::GREEN);
+            lines.line(transform.translation, transform.translation + normal, 0.0);
+        }
+
+        front.y += self.water_line - 0.2;
+        transform.look_at(front, normal);
+
+        transform.translation.y = ((front.y + left.y + right.y) / 3.0) + self.water_line;
+    }
+}
+
+pub fn update_ships(
+    water: WaterParam,
+    mut ships: Query<(&Ship, &mut Transform, &GlobalTransform)>,
+    #[cfg(feature = "debug")] mut lines: ResMut<DebugLines>,
+) {
+    for (ship, mut transform, global) in ships.iter_mut() {
+        let pos = global.translation();
+        #[cfg(not(feature = "debug"))]
+        ship.update(&water, pos, &mut transform);
+        #[cfg(feature = "debug")]
+        ship.update(&water, pos, &mut transform, &mut lines);
+    }
+}
+
+pub fn scale_uvs(mesh: &mut Mesh, scale: f32) {
+    match mesh.attribute_mut(Mesh::ATTRIBUTE_UV_0) {
+        Some(VertexAttributeValues::Float32x2(uvs)) => {
+            for [x, y] in uvs.iter_mut() {
+                *x *= scale;
+                *y *= scale;
+            }
+        }
+        Some(_) => {
+            panic!("Unexpected UV format");
+        }
+        _ => {
+            panic!("Mesh doesn't have UVS");
+        }
+    }
+}
+
+/// set up a simple ocean scene.
+pub fn setup_ocean(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // "Sun"
+    commands
+        .spawn((
+            DirectionalLight {
+                illuminance: 11127.65,
+                shadows_enabled: true,
+                ..default()
+            },
+            Transform::from_rotation(Quat::from_rotation_x(-0.340)),
+        ))
+        .insert(Sun); // Marks the light as Sun
+
+    // Prepare textures.
+    let base_color_texture = Some(asset_server.load("textures/coast_sand_01_1k/diff.jpg"));
+    let metallic_roughness_texture = Some(ImageReformat::reformat(
+        &mut commands,
+        &asset_server,
+        "textures/coast_sand_01_1k/rough.jpg",
+        TextureFormat::Rgba8Unorm,
+    ));
+    let normal_map_texture = Some(ImageReformat::reformat(
+        &mut commands,
+        &asset_server,
+        "textures/coast_sand_01_1k/normal.jpg",
+        TextureFormat::Rgba8Unorm,
+    ));
+    ImageReformat::uv_repeat(
+        &mut commands,
+        &asset_server,
+        "textures/coast_sand_01_1k/diff.jpg",
+    );
+    ImageReformat::uv_repeat(
+        &mut commands,
+        &asset_server,
+        "textures/coast_sand_01_1k/rough.jpg",
+    );
+    ImageReformat::uv_repeat(
+        &mut commands,
+        &asset_server,
+        "textures/coast_sand_01_1k/normal.jpg",
+    );
+
+    // Coast sand material.
+    let sandy = MeshMaterial3d(materials.add(StandardMaterial {
+        perceptual_roughness: 1.0,
+        metallic: 0.0,
+        reflectance: 0.5,
+        base_color_texture,
+        metallic_roughness_texture,
+        normal_map_texture,
+        cull_mode: None,
+        double_sided: true,
+        ..default()
+    }));
+
+    let floor_mesh = Mesh3d({
+        let mut mesh = PlaneMeshBuilder::from_length(256.0 * 6.0)
+            .subdivisions(25)
+            .build();
+        mesh.generate_tangents().expect("tangents");
+        scale_uvs(&mut mesh, 50.0);
+        meshes.add(mesh)
+    });
+    commands.spawn((
+        Name::new(format!("Sea floor")),
+        floor_mesh.clone(),
+        sandy.clone(),
+        Transform::from_xyz(0.0, -5.0, 0.0),
+    ));
+
+    let island_mesh = Mesh3d({
+        let mut mesh = Sphere::new(2.0)
+            .mesh()
+            .kind(SphereKind::Uv {
+                sectors: 90,
+                stacks: 60,
+            })
+            .build();
+        mesh.generate_tangents().expect("tangents");
+        scale_uvs(&mut mesh, 20.0);
+        meshes.add(mesh)
+    });
+    commands.spawn((
+        Name::new(format!("Sandy island")),
+        island_mesh.clone(),
+        sandy.clone(),
+        Transform::from_xyz(-30.0, -10.0, -30.0).with_scale(Vec3::new(30.0, 6.5, 30.0)),
+    ));
+}
+
+/// Create a simple Orb.
+pub fn setup_orb(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let orb_mesh = Mesh3d({
+        let mut mesh = Sphere::new(1.0)
+            .mesh()
+            .kind(SphereKind::Uv {
+                sectors: 90,
+                stacks: 60,
+            })
+            .build();
+        mesh.generate_tangents().expect("tangents");
+        meshes.add(mesh)
+    });
+    commands.spawn((
+        Name::new(format!("Orb")),
+        orb_mesh.clone(),
+        MeshMaterial3d(materials.add(Color::srgba(0.1, 0.2, 0.4, 1.0))),
+        Transform::from_xyz(-30.0, 10.0, -30.0),
+    ));
+}
+
+/// Create a simple 3D camera
+pub fn make_camera<'a>(
+    commands: &'a mut Commands,
+    asset_server: &AssetServer,
+) -> EntityCommands<'a> {
+    // camera
+    let mut cam = commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(-20.0, WATER_HEIGHT + 5.0, 20.0)
+            .looking_at(Vec3::new(0.0, WATER_HEIGHT, 0.0), Vec3::Y),
+        EnvironmentMapLight {
+            diffuse_map: asset_server
+                .load("environment_maps/table_mountain_2_puresky_4k_diffuse.ktx2"),
+            specular_map: asset_server
+                .load("environment_maps/table_mountain_2_puresky_4k_specular.ktx2"),
+            intensity: 1.0,
+            ..default()
+        },
+        DistanceFog {
+            color: Color::srgba(0.1, 0.2, 0.4, 1.0),
+            //directional_light_color: Color::srgba(1.0, 0.95, 0.75, 0.5),
+            //directional_light_exponent: 30.0,
+            falloff: FogFalloff::from_visibility_colors(
+                400.0, // distance in world units up to which objects retain visibility (>= 5% contrast)
+                Color::srgb(0.35, 0.5, 0.66), // atmospheric extinction color (after light is lost due to absorption by atmospheric particles)
+                Color::srgb(0.8, 0.844, 1.0), // atmospheric inscattering color (light gained due to scattering from the sun)
+            ),
+            ..default()
+        },
+    ));
+
+    //#[cfg(feature = "spectator")]
+    cam.insert(Spectator);
+
+    #[cfg(feature = "panorbit")]
+    cam.insert(PanOrbitCamera {
+        focus: Vec3::new(26.0, WATER_HEIGHT + 5.0, -11.0),
+        radius: Some(60.0),
+        yaw: Some(-std::f32::consts::FRAC_PI_2),
+        pitch: Some(0.0),
+        ..default()
+    });
+    {
+        cam.insert(Skybox {
+            image: asset_server.load("environment_maps/table_mountain_2_puresky_4k_cubemap.ktx2"),
+            brightness: 2000.0,
+            ..default()
+        });
+    }
+
+    /* #[cfg(feature = "depth_prepass")]
+    {
+      // This will write the depth buffer to a texture that you can use in the main pass
+      cam.insert(DepthPrepass);
+    }*/
+    // This is just to keep the compiler happy when not using `depth_prepass` feature.
+    cam.insert(Name::new("Camera"));
+
+    info!("Move camera around by using WASD for lateral movement");
+    info!("Use Left Shift and Spacebar for vertical movement");
+    info!("Use the mouse to look around");
+    info!("Press Esc to hide or show the mouse cursor");
+
+    cam
+}
+
+/// set up a simple 3D camera
+pub fn setup_camera(mut commands: Commands, asset_server: Res<AssetServer>) {
+    make_camera(&mut commands, &asset_server);
+}
+
+/// Spawn some dutch ships.
+pub fn setup_ships(mut commands: Commands, asset_server: Res<AssetServer>) {
+    // Spawn ships.
+    let scene = SceneRoot(
+        asset_server.load("models/dutch_ship_medium_1k/dutch_ship_medium_1k.gltf#Scene0"),
+    );
+    let ship = Ship::new(-0.400, -8.0, 9.0, -2.0, 2.0);
+
+    // "Randomly" place the ships.
+    for x in 1..10 {
+        let f = (x as f32) * 2.40;
+        let f2 = ((x % 6) as f32) * -20.90;
+        commands
+            .spawn((
+                ship.clone(),
+                Name::new(format!("Dutch Ship {x}")),
+                Transform::from_xyz(-10.0 + (f * 7.8), 0.0, 30.0 + f2)
+                    .with_rotation(Quat::from_rotation_y(f)),
+            ))
+            .with_children(|parent| {
+                parent.spawn((
+                    scene.clone(),
+                    // Rotate ship model to line up with rotation axis.
+                    Transform::from_rotation(Quat::from_rotation_y(std::f32::consts::FRAC_PI_2)),
+                ));
+            });
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,87 @@
+// basically just the bevy_water demo (https://github.com/Neopallium/bevy_water)
+//! Showcases dynamic ocean material + dynamic Sun/Atmosphere.
+//! Most of the daylight cycle code taken from the examples of `bevy_atmosphere`.
+
+use bevy::pbr::NotShadowCaster;
 use bevy::prelude::*;
+use bevy::render::mesh::*;
+
+mod app;
+use app::*;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+  let mut app = pirates_app("Simple ocean with ships");
+
+  // Setup
+  app.add_systems(
+    Startup,
+    (setup_simple_ocean, setup_camera, setup_simple_ships),
+  );
+
+  app.run();
+}
+
+/// Setup a simple ocean.
+fn setup_simple_ocean(
+  mut commands: Commands,
+  mut meshes: ResMut<Assets<Mesh>>,
+  mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+  // "Sun"
+  commands
+    .spawn((
+      DirectionalLight {
+        illuminance: 11127.65,
+        shadows_enabled: true,
+        ..default()
+      },
+      Transform::from_rotation(Quat::from_rotation_x(-0.340)),
+    ))
+    .insert(Sun); // Marks the light as Sun
+
+  // Terrain material.
+  let material = MeshMaterial3d(materials.add(StandardMaterial {
+    base_color: Color::srgba_u8(177, 168, 132, 255),
+    perceptual_roughness: 0.6,
+    metallic: 0.6,
+    reflectance: 0.8,
+    ..default()
+  }));
+
+  // Spawn simple terrain plane.
+  commands.spawn((
+    Name::new(format!("Terrain")),
+    Mesh3d(meshes.add(PlaneMeshBuilder::from_length(256.0 * 6.0))),
+    material.clone(),
+    Transform::from_xyz(0.0, -5.0, 0.0),
+    NotShadowCaster,
+  ));
+  // Spawn fake island.
+  commands.spawn((
+    Name::new(format!("Fake island")),
+    Mesh3d(meshes.add(Sphere::new(2.0))),
+    material.clone(),
+    Transform::from_xyz(-30.0, -10.0, -30.0).with_scale(Vec3::new(30.0, 6.5, 30.0)),
+    NotShadowCaster,
+  ));
+}
+
+/// Setup some simple ships.
+fn setup_simple_ships(mut commands: Commands, asset_server: Res<AssetServer>) {
+  // Spawn ships.
+  let scene = SceneRoot(asset_server.load("models/Kenney_pirate/ship_dark.gltf#Scene0"));
+  let ship = Ship::new(-0.400, -3.8, 2.5, -1.4, 1.4);
+
+  // "Randomly" place the ships.
+  for x in 1..18 {
+    let f = (x as f32) * 1.20;
+    let f2 = ((x % 6) as f32) * -10.90;
+    commands.spawn((
+      ship.clone(),
+      Name::new(format!("Ship {x}")),
+      scene.clone(),
+      Transform::from_xyz(-20.0 + (f * 7.8), 0.0, 20.0 + f2)
+        .with_rotation(Quat::from_rotation_y(f)),
+    ));
+  }
 }
